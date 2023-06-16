36f2a6d41f3a6a73d247806f0feaa552dcc02ab4
parmarkris <136467926+parmarkris@users.noreply.github.com>
2023-06-16 19:23:15 +0530

    day.19.c

--- tax1.c
+++ tax1.c
@@ -2,29 +2,45 @@
      
 int main()    
 {    
-    int n , i;
-    printf("Enter size of arry: ");
+    int n , i , m , j;
+    printf("Enter size of rows: ");
+    scanf("%d",&m);
+    
+    
+    printf("Enter size of column: ");
     scanf("%d",&n);
     
-    int a[n];
-	for(i=0 ; i<n ; i++) {
+    int a[m][n];
+	for(i=0 ; i<m ; i++) {
 		
-		printf("Enter elements of arry[%d] : ",i);
+		for(j=0 ; j<n ; j++){
 		
-		scanf("%d",&a[i]);
+		
+		
+		printf("Enter elements of arry[%d][%d] : ",i,j);
+		
+		scanf("%d",&a[i][j]);
+		
+	}
+}
+		for(i=0 ; i<m ; i++){
+			
+			for(j=0 ; j<n ; j++){
+					
+		    	printf("%d",a[i][j]);
+		   }
+		   printf("\n");
 	}
 		
-		for(i=0 ; i<n ; i++){
-			
-			
-			printf("%d",a[i]);
-		}
 		
+		return 0;
+	}
 		
 	   
     
-        
-    }      
+    
+      
      
         
       
+
--- tax6.c
+++ tax6.c
@@ -1,17 +1,58 @@
-#include<stdio.h>
-
-int main(){
-	
-	int r,c,k;
-	for(r=5 ; r>=1 ; r--){
-		for(k=1;k<r;k++){
-			printf(" ");
-		}
-		for(c=r; c<=5 ; c++){
-			printf("%d",r);
-		}
-		printf("\n");
+#include<stdio.h>    
+     
+int main()    
+{    
+    int n , i , m , j  ;
+    
+    printf("Enter size of rows: ");
+    scanf("%d",&m);
+    
+    
+    printf("Enter size of column: ");
+    scanf("%d",&n);
+    
+    int a[m][n];
+	for(i=0 ; i<m ; i++) {
+		
+		for(j=0 ; j<n ; j++){
+		
+		
+		
+		printf("Enter elements of arry[%d][%d] : ",i,j);
+		
+		scanf("%d",&a[i][j]);
+		
 	}
-	
-	return 0;
 }
+		for(i=0 ; i<m ; i++){
+			
+			for(j=0 ; j<n ; j++){
+					
+		    	printf("%d",a[i][j]);
+		   }
+		   printf("\n");
+	}
+		
+		int sum;
+		
+		 for(i=0 ; i<m ; i++)
+    {
+        
+        for(j=0 ; j<n ; j++)
+        {
+            sum += a[i][j];
+        }
+        printf("The Sum of Column Elements in a Matrix =  %d \n", sum );
+    }
+		
+		return 0;
+	}
+		
+	   
+    
+    
+      
+     
+        
+      
+
