059528d0f1082c11180a58a88674824413f3ca9f
parmarkris <136467926+parmarkris@users.noreply.github.com>
2023-07-08 17:14:34 +0530

    Add files via upload

---
+++ day 14/task 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+
+	int a,b,c;
+	printf("enter value a,b,c,d");
+	scanf("%d,&a,&b,&c,&d");
+	if(a>b){
+		printf("a is big");
+	}else{
+	
+	}if(b>c){
+		printf("b is big");
+	}else
+}
