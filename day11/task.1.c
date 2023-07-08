8ce77c79e38bdd6820ece4a9e8414dee711df23c
parmarkris <136467926+parmarkris@users.noreply.github.com>
2023-07-08 17:54:42 +0530

    Add files via upload

---
+++ day11/task.1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+ int main(){
+
+  int i =1;
+   int n;
+
+
+
+	 printf("Enter value :");
+	 scanf("%d",&i); 
+
+     while (i>=1){
+
+
+
+
+
+
+	   printf("%d\n",i);
+	   i--;
+
+	 }
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+
+ }
+
+
