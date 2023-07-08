ee3b125813b97da5d9f86b351da52d9847774c95
parmarkris <136467926+parmarkris@users.noreply.github.com>
2023-07-08 17:17:52 +0530

    Add files via upload

---
+++ day 1/Untitled3.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+ int main(){
+
+
+    printf("-\t-\t-\n");
+    printf("\t\t\n");
+    printf("J\t\t|\n");
+    printf("A\t\t\n"); 
+    printf("Y\t\t\n");
+    printf("E\t\t\n");
+    printf("S\t\t\n");
+    printf("H\t\t\n");
+    printf("-\t-\t-\n");
+
+    return 0;
+
+
+}
---
+++ day 1/task 1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+  int main(){
+
+
+     ("-\t-\t-\t-\t-\n");
+      printf("\t\t\t\t\n");printf("\t\t\t\t\n");
+      printf("\t\tjayesh\t\t\n");
+      printf("\t\t\t\t\n");
+     printf("\t\t\t\t\n"); printf("-\t-\t-\t-\t-\n");
+
+
+
+
+
+
+return 0;
+
+
+
+}
+
+
+
+
+
