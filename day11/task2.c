aa4701e7cd08231595e96583e51a93f6582de8cc
parmarkris <136467926+parmarkris@users.noreply.github.com>
2023-07-15 17:45:48 +0530

    Add files via upload

--- day11/task2.c
+++ day11/task2.c
@@ -1,42 +1,33 @@
-#include<stdio.h>
-
- int main(){
-
-  int i =1;
-   int n;
-
-
-
-	 printf("Enter value :");
-	 scanf("%d",&i); 
-
-     while (i>=1){
-
-
-
-
-
-
-	   printf("%d\n",i);
-	   i--;
-
-	 }
-
-
-
-
-
-
-
-
-
-
-
-
-  return 0;
-
- }
-
-
-
-
+#include <stdio.h>
+main(){
+    int a, b, c, d, vlog;
+    printf("\n Enter value to a, b, c, d: ");
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    if (a >b)
+    {
+        if(a>c)
+        {
+            if(a>d)
+            vlog = a;
+            else
+            vlog=d;
+        }
+        else
+           {
+           if(c>d)
+            vlog = c;
+            else
+            vlog = d;
+        }
+        else
+            if(b>c)
+            {
+                if(b>d)
+                vlog = b;
+                else
+                vlog = d;
+            }
+    }
+    printf("\n The biggest number is %d", vlog);
+    return 0;
+}
